748adbc046e1580dc70db3ee97b6bccccc241745
Olivia Gearner <olivia.gearner@gmail.com>
2021-06-28 21:57:20 -0500

    Added boilerplate. Cloud database connected but not yet in use

---
+++ photodex/app.js
@@ -0,0 +1,51 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+//vars for database connection
+const mongoose = require('mongoose');
+require('dotenv/config');
+const m = new mongoose.Mongoose( { useUnifiedTopology: true } );
+
+var app = express();
+
+m.connect(
+  process.env.DB_CONNECTION, 
+  { useNewUrlParser: true }, 
+  () => console.log('connected to DB!'));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
