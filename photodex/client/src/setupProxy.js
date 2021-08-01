78d0d176d87508c2b83c4dd99af73b11807a455b
Olivia Gearner <olivia.gearner@gmail.com>
2021-07-31 21:38:07 -0500

    fixed typo

--- photodex/client/src/setupProxy.js
+++ photodex/client/src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require("http-proxy-middleware");
 module.exports = function(app) {
   app.use(proxy("/api", { 
-    target: "http://localhost:8000",
+    target: "http://localhost:8000/",
     changeOrigin: true,
     secure: false,
     headers: {
