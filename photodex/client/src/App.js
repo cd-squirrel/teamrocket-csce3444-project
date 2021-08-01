01227a4a18be2161d03981fdf311f10ecc6af75d
FihoLee <86739498+FihoLee@users.noreply.github.com>
2021-07-31 23:56:12 -0500

    navbar

--- photodex/client/src/App.js
+++ photodex/client/src/App.js
@@ -5,7 +5,7 @@ What this returns will be placed where <div id="root"></div> is in public/index.
 */
 import React from 'react';
 import './App.css';
-import NavBar from './NavBar';
+import Navbar from './Navbar';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import MyAlbums from './pages/MyAlbums';
@@ -18,7 +18,7 @@ function App(){
   return (
     <Router>
       <div className="app">
-        <NavBar /> 
+        <Navbar /> 
         <div className="content container-flex m-4 d-flex justify-content-center" id ="album"> 
           <Switch>
             <Route exact path ="/">
