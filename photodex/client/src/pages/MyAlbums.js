9a03f51e24ce463e0cafbf48560c2bca3e623c18
FihoLee <86739498+FihoLee@users.noreply.github.com>
2021-07-31 22:29:30 -0500

    Update MyAlbums.js

--- photodex/client/src/pages/MyAlbums.js
+++ photodex/client/src/pages/MyAlbums.js
@@ -19,8 +19,9 @@ const MyAlbums = () => {
   }
   
   return (
-    <div className="album-list">
+     <div className="album-list">
       <img src="https://media.giphy.com/media/9V7qW099yYNyrgagqF/giphy.gif" id="bG"></img>
+
 
     <div class="contents">
       <h1>My Albums</h1>
